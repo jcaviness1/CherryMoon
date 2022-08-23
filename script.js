eef10c8567572619b7c0512a1738376e38fda1ef
jcaviness1 <109058376+jcaviness1@users.noreply.github.com>
2022-08-23 01:23:31 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function nuna(){
+    document.getElementById("text2").style.display="none";
+}
+function nana(){
+    document.getElementById("text3").innerHTML="Thank you <3";
+    document.getElementById("text3").style.color="peachpuff";
+    document.getElementById("text3").style.display = "block";
+    document.getElementById("cute").style.display = "none";
+}
+function nice(){
+    document.getElementById("anime").innerHTML="Aww really? Hehe, okay, choose 4 me <3";
+    document.getElementById("anime").style.color="peachpuff";
+    document.getElementById("bibi").style.display="none";
+    document.getElementById("no").style.display="none";
+    document.getElementById("cat").style.display="none";
+}
+function wow(){
+    document.getElementById("manga").innerHTML ="Okay :(";
+    document.getElementById("manga").style.color="peachpuff";
+    document.getElementById("no").style.display="none";
+    document.getElementById("bibi").style.display="none";
+    document.getElementById("cat").style.display="none";
+}
+function mist2(){
+    document.getElementById("btn2").style.display="block";
+    document.getElementById("pyt").style.display="block";
+}
+function mist3(){
+    document.getElementById("mist").innerHTML="Can't wait to meet hehe ;)";
+    document.getElementById("mist").style.color="peachpuff";
+    document.getElementById("btn2").style.display="none";
+    document.getElementById("pyt").style.display="none";
+    document.getElementById("anime").style.display="none";
+}
+function show() {
+    document.getElementById('image')
+            .style.display = "block";
+
+    document.getElementById('btnID')
+            .style.display = "none";
+}
+function myFunction(){
+    document.getElementById("text1").innerHTML="Thank you! Me too ;)";
+    document.getElementById("text1").style.color="peachpuff";
+    document.getElementById("star").style.display="none";
+    document.getElementById("cafe").style.display="block";
+    document.getElementById("moon").style.display="none";
+}
+function myFunction1(){
+    document.getElementById("text1").innerHTML ="That's too bad :(";
+    document.getElementById("text1").style.color="peachpuff";
+    document.getElementById("moon").style.display="none";
+    document.getElementById("star").style.display="none";
+    document.getElementById("mc").style.display="block";
+
+
+}
